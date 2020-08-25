0681d1040b7ff81ba4100db2f3dc516dbe1e2f2d
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-08-25 16:53:17 -0400

    when creating a new json the region-count json file will keep a running total of how many pages have been scraped of a given region

--- createJson.py
+++ createJson.py
@@ -2,8 +2,18 @@ import json
 # def createJsonFile(fileName, dict):
 #     with open(fileName, "w", encoding="utf8") as outfile:
 #         json.dump(dict, outfile, ensure_ascii=False)
-def createJsonFile(fileName, bizs):
+def createJsonFile(counter, data, dev, region):
+    
+    fileName = f"{region}-{counter}"
+    with open('./region-count.json') as regionCount:
+        rCount = json.load(regionCount)
+    
+    rCount[dev][region] = counter
+
+    with open('./region-count.json', "w") as modRCount:
+        json.dump(rCount, modRCount, ensure_ascii=False)
+
     with open(f"scraped/{fileName}.json", "w") as outfile:
-        json.dump({ "bizs" : bizs }, outfile, ensure_ascii=False)
+        json.dump({ "bizs" : data }, outfile, ensure_ascii=False)
 
         
