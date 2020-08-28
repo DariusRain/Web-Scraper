8605149ab77c24c7c458101db59c5a22e2e85032
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-08-27 22:17:28 -0400

    update js script to compile all scraped data

--- readJson.js
+++ readJson.js
@@ -1,9 +1,41 @@
 const fs = require('fs')
-const data = fs.readFileSync('./scraped/bizs-52.json', 'utf-8');
 
-const json = JSON.parse(data);
+const directoryPath = process.cwd() + '\\all-data';
 
-json.bizs.forEach(elm => {
-    obj = JSON.parse(elm)
-    console.log(elm);
+console.log(directoryPath);
+const allJSON = [];
+
+let count = 0;
+
+fs.readdir(directoryPath, function (err, files) {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    files.forEach( async (file) => {
+
+        const filePath = process.cwd() + '\\all-data\\' + file;
+            
+            try {
+                const rawTxt = fs.readFileSync(filePath, 'utf-8');
+                
+                const parsed = JSON.parse(rawTxt);
+                if (parsed.bizs === undefined) console.log('caught', file);
+                parsed.bizs.forEach(data => {
+                    const parsedBiz = JSON.parse(data);
+                    allJSON.push(parsedBiz)                 
+                });
+            } catch (err) {
+                console.log(err.message, file);
+            }
+           
+    });
+    writeJSONFile(allJSON)
+
 });
+
+function writeJSONFile(jsonArr) {
+
+    const stringified = JSON.stringify(jsonArr)
+
+    fs.writeFileSync('compiledJSON.json', stringified)
+}
