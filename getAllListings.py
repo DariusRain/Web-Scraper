e3363a357725fb6a3b3a4ac1c097cbe8460fb912
DariusRain <dariusrain.student@careerdevs.com>
2020-10-08 16:24:12 -0400

    Got a good clean version here, finished up the scrape.

--- getAllListings.py
+++ getAllListings.py
@@ -1,71 +1,173 @@
-# Attempting to do an all in one, good suggestion @GaberialSherman
 import json
-import requests
 import time
 from addressParser import stateParser
-from bs4 import BeautifulSoup
-from secrets import urlOfAllListings, pathOfListingItems, pathToBuisinessName,pathToAddress, pathToCategories, pathToImage, pathToExtraItem, pathToDescription, pathToPhoneNumber, pathToWebsite,  pathToEmail, html_doc1, urlBase
-
+from secrets import *
 from createJson import createJsonFile
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options 
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+# import requests
+# from bs4 import BeautifulSoup
+options = Options()
+options.headless = True
+options.add_argument("--window-size=1920,1200")
 
-#  NOTE 8/25 urlOfAllListings is no longer need now using list of urls
+driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 
-# This function returns a dictrionary see README for structured example.
+# driver.quit()
 def getListings():
-    curPage = []
-    response = None
-    # response = requests.get(urlOfAllListings)      
-    soup = BeautifulSoup(html_doc1, "lxml")
     
-    print(f"Scraping: {urlOfAllListings}")
-    
-    # For each item 
-    for item in soup.find_all("div", pathOfListingItems):
-        buisinessName = item.select(pathToBuisinessName)
-        image = item.select(pathToImage)
-        address = item.select(pathToAddress)
-        newBiz = type("NewBiz", (object,), {})()
-
-
-        urlOfExtraItem = urlBase + item.find("a", pathToExtraItem)["href"]
-        print(f"Scraping: {urlOfExtraItem}")
-        response2 = requests.get(urlOfExtraItem)
-        soup2 = BeautifulSoup(response2.text, "lxml")
-        print(soup2.find_all("div", {"class": "kn-label-top kn-table is-unbordered"}))
-        description = soup2.select(pathToDescription)
-        phoneNumber = soup2.select(pathToPhoneNumber)
-        website = soup2.select(pathToWebsite)
-        email = soup2.select(pathToEmail)
-        links = soup2.find_all("a")
-        # if not len(buisinessName) == 0:
-        print("BuisinessName", buisinessName)
-        #     newBiz.name = buisinessName[0].get_text().strip()
-
-        # if not len(phoneNumber) == 0:
-        print("PhoneNumber", phoneNumber)
-        #     newBiz.phoneNumber = phoneNumber[0].get_text().strip()
-        
-        # if not len(description) == 0:
-        print("Description", description)
-        #     newBiz.description = description[0].get_text().strip()
-        
-        # if not len(website) == 0:
-        print("Website", website)
-        #     newBiz.website = website[0]
-        
-        # if not len(image) == 0:
-        print("Image", image)
-        #     newBiz.image = image[0].strip()
-
-        # if not len(address) == None:
-        print("Address", address)
-        #     newBiz.address = stateParser(address[0])["address"]
-        print("Email", email)
-
-        print("Links", links)
-        # curPage.append(json.dumps(newBiz.__dict__))    
+    currPage = []
+    pageCount = 0
+    counter = 1
+    linkCount = 0
+    driver.get(urlOfAllListings + str(pageCount))
+    while(len(driver.find_elements(By.TAG_NAME, "div")) != 0):    
+        try:  
+            listing = driver.find_elements(By.CSS_SELECTOR, pathToListing)
+            newBiz = type("NewBiz", (object,), {})()
             
-    print("Done...")
+            # Debug 
+            # print(listing.text)            
+            
+            # print(counter, len(listing))
+            for item in listing:
+                try:
+                    newBiz.categories = []
+                    newBiz.contact = []
+                    phoneNumber = item.find_element(By.CSS_SELECTOR, pathToPhoneNumber).text.replace(".", "")
+                    newBiz.contact.append({ "type": "phoneNumber", "data": phoneNumber })
+                    newBiz.name = item.find_element(By.CSS_SELECTOR, pathToBuisinessName).text.split(". ")[1]
+                    newBiz.description = item.find_element(By.TAG_NAME, "p").text
+                    categoriesList = item.find_element(By.CSS_SELECTOR, pathToCategories).find_elements(By.TAG_NAME, "a")
+                    
+                    for category in categoriesList:
+                        newBiz.categories.append(category.text)
+                    
+                    contactList = item.find_element(By.CSS_SELECTOR, pathToContact).find_elements(By.TAG_NAME, "a")
+
+                    for contactType in contactList:
+                        propName = contactType.get_attribute("title").lower()
+                        if not propName == "":
+                            linkTo = contactType.get_attribute("href").replace("mailto:", "")
+                            newBiz.contact.append({ "type":  propName, "data": linkTo}) 
+                    
+                    newBiz.address = item.find_element(By.CSS_SELECTOR, pathToAddress).text
+
+                except NoSuchElementException:
+                    print("Had an issue getting feild")
+                    
+                
+                finally:
+                    currPage.append(json.dumps(newBiz.__dict__))   
+                    print("currPage length:", len(currPage))
+        finally:
+            
+            # Debug
+            # print("name:", newBiz.name)
+            # print("address:", newBiz.address)
+            # print("description:", newBiz.description)
+            # print("categories:", newBiz.categories)
+            # print("contact:", newBiz.contact)
+            # print("\n\nSleeping...")
+
+            createJsonFile(counter, currPage)
+            time.sleep(3)
+            pageCount += 10
+            counter += 1
+            currPage.clear()
+            driver.get(urlOfAllListings + str(pageCount))
+    driver.quit()
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Scrape 2
+# #  NOTE 8/25 urlOfAllListings is no longer need now using list of urls
+
+# # This function returns a dictrionary see README for structured example.
+# def getListings():
+#     curPage = []
+#     response = None
+#     # response = requests.get(urlOfAllListings)      
+#     soup = BeautifulSoup(html_doc1, "lxml")
+    
+#     print(f"Scraping: {urlOfAllListings}")
+    
+#     # For each item 
+#     for item in soup.find_all("div", pathOfListingItems):
+#         buisinessName = item.select(pathToBuisinessName)
+#         image = item.select(pathToImage)
+#         address = item.select(pathToAddress)
+#         newBiz = type("NewBiz", (object,), {})()
+
+
+#         urlOfExtraItem = urlBase + item.find("a", pathToExtraItem)["href"]
+#         print(f"Scraping: {urlOfExtraItem}")
+#         response2 = requests.get(urlOfExtraItem)
+#         soup2 = BeautifulSoup(response2.text, "lxml")
+#         print(soup2.find_all("div", {"class": "kn-label-top kn-table is-unbordered"}))
+#         description = soup2.select(pathToDescription)
+#         phoneNumber = soup2.select(pathToPhoneNumber)
+#         website = soup2.select(pathToWebsite)
+#         email = soup2.select(pathToEmail)
+#         links = soup2.find_all("a")
+#         # if not len(buisinessName) == 0:
+#         print("BuisinessName", buisinessName)
+#         #     newBiz.name = buisinessName[0].get_text().strip()
+
+#         # if not len(phoneNumber) == 0:
+#         print("PhoneNumber", phoneNumber)
+#         #     newBiz.phoneNumber = phoneNumber[0].get_text().strip()
+        
+#         # if not len(description) == 0:
+#         print("Description", description)
+#         #     newBiz.description = description[0].get_text().strip()
+        
+#         # if not len(website) == 0:
+#         print("Website", website)
+#         #     newBiz.website = website[0]
+        
+#         # if not len(image) == 0:
+#         print("Image", image)
+#         #     newBiz.image = image[0].strip()
+
+#         # if not len(address) == None:
+#         print("Address", address)
+#         #     newBiz.address = stateParser(address[0])["address"]
+#         print("Email", email)
+
+#         print("Links", links)
+#         # curPage.append(json.dumps(newBiz.__dict__))    
+            
+#     print("Done...")
+
+
+
+
     # createJsonFile(1, curPage)
 
     # return curPage
